258d89e504acb8d16c69c43bc74d9719d37fe8ac
Somesheggy <sheggyAutomation@gmail.com>
2018-06-25 05:48:15 +0100

    New class (SummaryPage) with dummie elements N.B: Can properly locate the element

---
+++ src/test/java/com/auto/page/object/SummaryPage.java
@@ -0,0 +1,40 @@
+package com.auto.page.object;
+
+
+import com.auto.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+
+public class SummaryPage {
+
+    private WebDriver driver;
+
+
+    @FindBy(name = "Registration")
+    public WebElement RegNos;
+    @FindBy(name = "Maker")
+    public WebElement Make;
+    @FindBy(name = "Color")
+    public WebElement Colour;
+    @FindBy(name = "RegistrationLabel")
+    public WebElement RegNosLab;
+    @FindBy(name = "MakeLabel")
+    public WebElement MakeLab;
+    @FindBy(name = "ColorLabel")
+    public WebElement ColourLab;
+
+
+
+    public SummaryPage(Driver driver) throws IOException {
+
+        WebDriver sdriver = driver.start();
+        this.driver = sdriver;
+        PageFactory.initElements(sdriver, this);
+
+    }
+
+}
